b180dcacd96099cdb4a09c4db54bdd8bdfa5ff7a
N1ks0n1337 <sirvach666@gmail.com>
2022-05-03 15:03:35 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        freeMode: true,
+        breakpoints: {
+            1264: {
+                slidesPerView: 4,  
+            },
+            
+            884: {
+                slidesPerView: 3,  
+            },
+            764:{
+                slidesPerView: 3.5,  
+            },
+            500:{
+                slidesPerView: 3,  
+            },
+            417: {
+                slidesPerView: 2.3,  
+            },
+            320: {
+                slidesPerView: 2,  
+            },
+            0: {
+                slidesPerView: 1,  
+            },
+        }
+        
+      });
+      var swiper = new Swiper(".stockSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        breakpoints: {
+            1264: {
+                slidesPerView: 2.7,  
+            },
+            
+            884: {
+                slidesPerView: 2,  
+            },
+            764:{
+                slidesPerView: 2,  
+            },
+            500:{
+                slidesPerView: 1,  
+            },
+            320: {
+                slidesPerView: 1,  
+            },
+            0: {
+                slidesPerView: 1,  
+            },
+        }
+        
+      });
+      
